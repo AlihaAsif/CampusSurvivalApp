8a8dadd85b52c5f575287fd717eda9fa7ef84292
Aliha <alihaasif1234@gmail.com>
2026-08-21 16:24:17 +0500

    Stop tracking config files with secrets

--- lib/core/config/supabase_config.dart
+++
@@ -1,8 +0,0 @@
-class SupabaseConfig {
-  SupabaseConfig._();
-
-  static const String url = 'https://kgxqmbzbdlgkzqjijbnh.supabase.co';
-  static const String anonKey = 'sb_publishable_ir2h-L9zvT8oyaXLOp2gvg_Iw4UwgAt';
-
-  static const String lostItemsBucket = 'lost-items';
-}
